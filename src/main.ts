672fdfbd41510087f950db44c4b7702254593d0d
Gabriel Pretel <gabrilinex@gmail.com>
2024-01-19 19:50:13 +0100

    Formateo

--- src/main.ts
+++ src/main.ts
@@ -1,88 +1,79 @@
-interface Grupos{
-    nombre: string,
-    fundacion: number,
-    activo: boolean,
-    genero: string,
+interface Grupos {
+  nombre: string;
+  fundacion: number;
+  activo: boolean;
+  genero: string;
 }
 
-const popRock : string = "🎵 Pop Rock";
-const rock : string = "🎸 Rock";
-const hardRock : string = "🤘 Hard Rock";
-const clasica : string = "🎼 Clásica";
+const popRock: string = "🎵 Pop Rock";
+const rock: string = "🎸 Rock";
+const hardRock: string = "🤘 Hard Rock";
+const clasica: string = "🎼 Clásica";
 
-const formato : string = "background-color: green; font-size:18px; font-weight:bold";
+const formato: string =
+  "background-color: green; font-size:18px; font-weight:bold";
 
-
-const grupoA : Grupos = {
-    nombre: "The Beatles",
-    fundacion: 1960,
-    activo: true,
-    genero: popRock,
+const grupoA: Grupos = {
+  nombre: "The Beatles",
+  fundacion: 1960,
+  activo: true,
+  genero: popRock,
 };
 
-console.log(`%c${grupoA.nombre}`,formato);
+console.log(`%c${grupoA.nombre}`, formato);
 console.log(` 
 Año de fundación: ${grupoA.fundacion}
 Activo: ${grupoA.activo}
-Género:${grupoA.genero}`
-);
+Género:${grupoA.genero}`);
 
-
-const grupoB : Grupos = {
-    nombre: "Queen",
-    fundacion: 1970,
-    activo: false,
-    genero: rock,
+const grupoB: Grupos = {
+  nombre: "Queen",
+  fundacion: 1970,
+  activo: false,
+  genero: rock,
 };
 
-console.log(`%c${grupoB.nombre}`,formato);
+console.log(`%c${grupoB.nombre}`, formato);
 console.log(` 
 Año de fundación: ${grupoB.fundacion}
 Activo: ${grupoB.activo}
-Género:${grupoB.genero}`
-);
+Género:${grupoB.genero}`);
 
-
-const grupoC : Grupos = {
-    nombre: "AC DC",
-    fundacion: 1973,
-    activo: true,
-    genero: hardRock,
+const grupoC: Grupos = {
+  nombre: "AC DC",
+  fundacion: 1973,
+  activo: true,
+  genero: hardRock,
 };
 
-console.log(`%c${grupoC.nombre}`,formato);
+console.log(`%c${grupoC.nombre}`, formato);
 console.log(` 
 Año de fundación: ${grupoC.fundacion}
 Activo: ${grupoC.activo}
-Género:${grupoC.genero}`
-);
+Género:${grupoC.genero}`);
 
-
-const grupoD : Grupos = {
-    nombre: "Ludwig van Beethoven",
-    fundacion: 1770,
-    activo: false,
-    genero: clasica,
+const grupoD: Grupos = {
+  nombre: "Ludwig van Beethoven",
+  fundacion: 1770,
+  activo: false,
+  genero: clasica,
 };
 
-console.log(`%c${grupoD.nombre}`,formato);
+console.log(`%c${grupoD.nombre}`, formato);
 console.log(` 
 Año de fundación: ${grupoD.fundacion}
 Activo: ${grupoD.activo}
-Género:${grupoD.genero}`
-);
+Género:${grupoD.genero}`);
 
-
-const grupoE : Grupos = {
-    nombre: "The Rolling Stones",
-    fundacion: 1962,
-    activo: true,
-    genero: rock,
+const grupoE: Grupos = {
+  nombre: "The Rolling Stones",
+  fundacion: 1962,
+  activo: true,
+  genero: rock,
 };
 
-console.log(`%c${grupoE.nombre}`,formato);
+console.log(`%c${grupoE.nombre}`, formato);
 console.log(` 
 Año de fundación: ${grupoE.fundacion}
 Activo: ${grupoE.activo}
-Género:${grupoE.genero}`
-);
+Género:${grupoE.genero}`);
